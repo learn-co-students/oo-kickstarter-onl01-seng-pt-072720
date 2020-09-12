841bbf8a3114c49d0575feafe572aa4de510f216
Danniely Santos <dannielyingrid@gmail.com>
2020-09-12 04:21:48 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_reader :backers
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    #backer.add_project(self)
+  end
+end
